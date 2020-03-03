13a2d872265a509b3ceb739033147583f0aadcf5
Almetica <almetica@protonmail.com>
2020-03-03 19:15:32 +0100

    Fix some crypto errors.
    Encryption / Decryption still seem to have some errors.
    Will test against the JS version.

--- src/crypt.rs
+++ src/crypt.rs
@@ -37,19 +37,19 @@ impl Cryptor {
             sha.update(&expanded_key);
             let hash = sha.hash().unwrap();
             for j in (0..20).step_by(4) {
-                LittleEndian::write_u32(&mut expanded_key[i + j..], hash[j]);
+                LittleEndian::write_u32(&mut expanded_key[i + j..], hash[j / 4]);
             }
         }
 
         // Create the cryptor keys out of the expanded key
         for i in 0..55 {
-            c.keys[0].buffer[i] = BigEndian::read_u32(&expanded_key[i..]);
+            c.keys[0].buffer[i] = LittleEndian::read_u32(&expanded_key[i * 4..]);
         }
         for i in 0..57 {
-            c.keys[1].buffer[i] = BigEndian::read_u32(&expanded_key[(i * 4 + 220)..]);
+            c.keys[1].buffer[i] = LittleEndian::read_u32(&expanded_key[(i * 4 + 220)..]);
         }
         for i in 0..58 {
-            c.keys[2].buffer[i] = BigEndian::read_u32(&expanded_key[(i * 4 + 448)..]);
+            c.keys[2].buffer[i] = LittleEndian::read_u32(&expanded_key[(i * 4 + 448)..]);
         }
         c
     }
@@ -76,9 +76,9 @@ impl Cryptor {
             self.do_round();
             for k in self.keys.iter() {
                 data[i] ^= k.sum as u8;
-                data[i+1] ^= (k.sum >> 8) as u8;
-                data[i+2] ^= (k.sum >> 16) as u8;
-                data[i+3] ^= (k.sum >> 24) as u8;
+                data[i + 1] ^= (k.sum >> 8) as u8;
+                data[i + 2] ^= (k.sum >> 16) as u8;
+                data[i + 3] ^= (k.sum >> 24) as u8;
             }
         }
 
@@ -91,7 +91,7 @@ impl Cryptor {
             }
 
             for i in 0..remain {
-                data[size-remain+i] ^= (self.change_data >> (i * 8)) as u8;
+                data[size - remain + i] ^= (self.change_data >> (i * 8)) as u8;
             }
 
             self.change_len = 4 - remain;
@@ -107,10 +107,10 @@ impl Cryptor {
                 let t1 = k.buffer[k.pos1 as usize];
                 let t2 = k.buffer[k.pos2 as usize];
                 let t3 = if t1 <= t2 { t1 } else { t2 };
-                k.sum = t1 + t2;
+                k.sum = t1.wrapping_add(t2);
                 k.key = if t3 > k.sum { 1 } else { 0 };
-                k.pos1 = (k.pos1+1) % k.size as u32;
-                k.pos2 = (k.pos2+1) % k.size as u32;
+                k.pos1 = (k.pos1 + 1) % k.size as u32;
+                k.pos2 = (k.pos2 + 1) % k.size as u32;
             }
         }
     }
@@ -160,13 +160,13 @@ impl CryptorSession {
 
         shift_key(&mut tmp1, &server_keys[0], -67);
         xor_key(&mut tmp2, &tmp1, &client_keys[0]);
+
         shift_key(&mut tmp1, &client_keys[1], 29);
         xor_key(&mut tmp3, &tmp1, &tmp2);
         let mut decryptor = Cryptor::new(&tmp3);
 
         shift_key(&mut tmp1, &server_keys[1], -41);
         decryptor.apply(&mut tmp1);
-
         let encryptor = Cryptor::new(&tmp1);
 
         let cs = CryptorSession {
@@ -187,13 +187,12 @@ impl CryptorSession {
     }
 }
 
-fn shift_key(dst: &mut [u8], src: &[u8], n: i8) {
+fn shift_key(dst: &mut [u8], src: &[u8], n: i32) {
+    dst.copy_from_slice(src);
     if n > 0 {
-        dst.copy_from_slice(&src[0..n as usize]);
-        dst.copy_from_slice(&src[(128 - n as usize)..]);
+        dst.rotate_right(n as usize)
     } else {
-        dst.copy_from_slice(&src[0..(128 + n as usize)]);
-        dst.copy_from_slice(&src[-n as usize..]);
+        dst.rotate_left(-n as usize)
     }
 }
 
@@ -205,5 +204,50 @@ fn xor_key(dst: &mut [u8], key1: &[u8], key2: &[u8]) {
 
 #[cfg(test)]
 mod tests {
-    // TODO
+    use super::CryptorSession;
+    use hex::encode;
+
+    #[test]
+    fn test_empty_keys() {
+        let c1: [u8; 128] = [0; 128];
+        let c2: [u8; 128] = [0; 128];
+        let s1: [u8; 128] = [0; 128];
+        let s2: [u8; 128] = [0; 128];
+
+        let mut session = CryptorSession::new([c1, c2], [s1, s2]);
+
+        let org: [u8; 32] = [0; 32];
+        let mut data: [u8; 32] = org;
+
+        session.encrypt(&mut data);
+        //assert_eq!("bf8f6ffee1c8a998560adb213307de236de34a5477589ca0440ea49147cb82dc", encode(&data));
+
+        session.decrypt(&mut data);
+        //assert_eq!("827ec6087a1def309670c52a7cfa6140f98a46e476daa003148b725ed624470d", encode(&data));
+
+        // TODO Gives same values as the GO version. Maybe the encryption / decryption needs different key sets (server <-> client keys?)
+        assert_eq!(encode(&org), encode(&data));
+    }
+
+    #[test]
+    fn test_full_keys() {
+        let c1: [u8; 128] = [0xff; 128];
+        let c2: [u8; 128] = [0xff; 128];
+        let s1: [u8; 128] = [0xff; 128];
+        let s2: [u8; 128] = [0xff; 128];
+
+        let mut session = CryptorSession::new([c1, c2], [s1, s2]);
+
+        let org: [u8; 32] = [0; 32];
+        let mut data: [u8; 32] = org;
+
+        session.encrypt(&mut data);
+        //assert_eq!("e264594c4e792ba95da3d81572f9ecea729b007bec661226465139096b07a624", encode(&data));
+
+        session.decrypt(&mut data);
+        //assert_eq!("98da7773e4531b4cf2aba5a03e5cab3369fc81fbc25e0d2b35ae4fcaff47e3f4", encode(&data));
+
+        // TODO Gives same values as the GO version. Maybe the encryption / decryption needs different key sets (server <-> client keys?)
+        assert_eq!(encode(&org), encode(&data));
+    }
 }
