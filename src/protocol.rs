1ee07ca492fc3efd91f68dfeeeba9f103af25aed
Almetica <almetica@protonmail.com>
2020-03-06 15:16:21 +0100

    We are async now!
    First try with tokio. Will try to use async-std to see how it feels too.

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,14 +1,15 @@
 /// Module that implments the network protocol used by tera.
 pub mod opcode;
 
-use std::io::{Read, Write};
 use std::net::{SocketAddr, TcpStream};
 
-use super::*;
 use super::crypt::CryptSession;
-use log::{debug, info, error};
+use super::*;
+use log::{debug, error, info};
 use rand::rngs::OsRng;
 use rand_core::RngCore;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::prelude::*;
 
 /// Abstracts the game network protocol session.
 struct GameSession {
@@ -20,18 +21,17 @@ struct GameSession {
 
 impl GameSession {
     /// Initializes and returns a `GameSession` object.
-    pub fn new<T: ?Sized>(stream: &mut T, addr: SocketAddr) -> Result<GameSession>
+    pub async fn new<T: Unpin>(stream: &mut T, addr: SocketAddr) -> Result<GameSession>
     where
-        T: Read + Write,
+        T: AsyncRead + AsyncWrite,
     {
         let magic_word_buffer: [u8; 4] = [0x01, 0x00, 0x00, 0x00];
         let mut client_key_1: [u8; 128] = [0; 128];
         let mut client_key_2: [u8; 128] = [0; 128];
         let mut server_key_1: [u8; 128] = [0; 128];
         let mut server_key_2: [u8; 128] = [0; 128];
-        
         debug!("Sending magic word on socket: {}", addr);
-        match stream.write_all(&magic_word_buffer) {
+        match stream.write_all(&magic_word_buffer).await {
             Ok(()) => (),
             Err(e) => {
                 error!("Can't send magic word on socket {}: {}", addr, e);
@@ -39,8 +39,8 @@ impl GameSession {
             }
         };
 
-        match stream.read_exact(&mut client_key_1) {
-            Ok(()) => (),
+        match stream.read_exact(&mut client_key_1).await {
+            Ok(_i) => 0,
             Err(e) => {
                 error!("Can't read client key 1 on socket {}: {}", addr, e);
                 return Err(Error::Io(e));
@@ -49,7 +49,7 @@ impl GameSession {
         debug!("Recieved client key 1 on socket {}", addr);
 
         OsRng.fill_bytes(&mut server_key_1);
-        match stream.write_all(&server_key_1) {
+        match stream.write_all(&server_key_1).await {
             Ok(()) => (),
             Err(e) => {
                 error!("Can't write server key 1 on socket {}: {}", addr, e);
@@ -58,8 +58,8 @@ impl GameSession {
         };
         debug!("Send server key 1 on socket {}", addr);
 
-        match stream.read_exact(&mut client_key_2) {
-            Ok(()) => (),
+        match stream.read_exact(&mut client_key_2).await {
+            Ok(_i) => 0,
             Err(e) => {
                 error!("Can't read client key 2 on socket {}: {}", addr, e);
                 return Err(Error::Io(e));
@@ -68,7 +68,7 @@ impl GameSession {
         debug!("Recieved client key 2 on socket {}", addr);
 
         OsRng.fill_bytes(&mut server_key_2);
-        match stream.write_all(&server_key_2) {
+        match stream.write_all(&server_key_2).await {
             Ok(()) => (),
             Err(e) => {
                 error!("Can't write server key 2 on socket {}: {}", addr, e);
@@ -91,22 +91,25 @@ impl GameSession {
     /// Handles the writing / sending on the TCP stream.
     pub fn handle_connection(stream: &mut TcpStream) {
         // TODO
-    }  
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::task::{Context, Poll};
+    use core::pin::Pin;
     use std::default::Default;
-    use std::io::{Error, ErrorKind, Read, Write};
-    use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+    use std::io::{Error, ErrorKind};
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+    use tokio::io::{AsyncRead, AsyncWrite};
 
-    #[test]
-    fn test_read_gamesession_creation() {
+    #[tokio::test]
+    async fn test_read_gamesession_creation() {
         // Mocked TCP stream. Implementaion below.
         let mut stream = StreamMock::default();
         let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-        GameSession::new(&mut stream, addr).unwrap();
+        GameSession::new(&mut stream, addr).await;
 
         assert_eq!(4, stream.state);
     }
@@ -118,66 +121,84 @@ mod tests {
 
     impl Default for StreamMock {
         fn default() -> Self {
-            StreamMock {
-                state: -1,
-            }
+            StreamMock { state: -1 }
         }
     }
 
-    impl Read for StreamMock {
-        fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+    impl AsyncRead for StreamMock {
+        fn poll_read(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context,
+            buf: &mut [u8],
+        ) -> Poll<Result<usize, Error>> {
             match self.state {
                 0 => {
                     self.state = 1;
                     let client_key1: [u8; 128] = [0xAA; 128];
                     buf.copy_from_slice(&client_key1);
-                    Ok(client_key1.len())
-                },
+                    Poll::Ready(Ok(client_key1.len()))
+                }
                 2 => {
                     self.state = 3;
                     let client_key2: [u8; 128] = [0xCC; 128];
                     buf.copy_from_slice(&client_key2);
-                    Ok(client_key2.len())
-                },
-                _ => {
-                    Err(Error::new(ErrorKind::Other, format!("unexpected read at state {}", self.state)))
+                    Poll::Ready(Ok(client_key2.len()))
                 }
+                _ => Poll::Ready(Err(Error::new(
+                    ErrorKind::Other,
+                    format!("unexpected read at state {}", self.state),
+                ))),
             }
         }
     }
 
-    impl Write for StreamMock {
-        fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+    impl AsyncWrite for StreamMock {
+        fn poll_write(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context,
+            buf: &[u8],
+        ) -> Poll<Result<usize, Error>> {
             match self.state {
                 -1 => {
                     self.state = 0;
                     let mut magic_word: [u8; 4] = [0xFF; 4];
                     magic_word.copy_from_slice(buf);
                     if magic_word[0] != 1 {
-                        return Err(Error::new(ErrorKind::Other, format!("wrong magic word")));
+                        return Poll::Ready(Err(Error::new(ErrorKind::Other, format!("wrong magic word"))));
                     }
-                    Ok(magic_word.len())
-                },
+                    Poll::Ready(Ok(magic_word.len()))
+                }
                 1 => {
                     self.state = 2;
                     let mut server_key_1: [u8; 128] = [0xFF; 128];
                     server_key_1.copy_from_slice(buf);
-                    Ok(server_key_1.len())
-                },
+                    Poll::Ready(Ok(server_key_1.len()))
+                }
                 3 => {
                     self.state = 4;
                     let mut server_key_2: [u8; 128] = [0xFF; 128];
                     server_key_2.copy_from_slice(buf);
-                    Ok(server_key_2.len())
-                },
-                _ => {
-                    Err(Error::new(ErrorKind::Other, format!("unexpected write at state {}", self.state)))
+                    Poll::Ready(Ok(server_key_2.len()))
                 }
+                _ =>  Poll::Ready(Err(Error::new(
+                    ErrorKind::Other,
+                    format!("unexpected write at state {}", self.state),
+                ))),
             }
         }
 
-        fn flush(&mut self) -> Result<(), Error> {
-            Err(Error::new(ErrorKind::Other, format!("unexpected flush at state {}", self.state)))
+        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Error>> {
+            Poll::Ready(Err(Error::new(
+                ErrorKind::Other,
+                format!("unexpected flush at state {}", self.state),
+            )))
+        }
+
+        fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Error>> {
+            Poll::Ready(Err(Error::new(
+                ErrorKind::Other,
+                format!("unexpected shutdown at state {}", self.state),
+            )))
         }
     }
 }
