0895a706522f36a8529c376446247a29b1916a09
Almetica <almetica@protonmail.com>
2020-03-27 16:08:37 +0100

    Implement a simple login server.

--- src/bin/almetica-server.rs
+++ src/bin/almetica-server.rs
@@ -2,7 +2,7 @@
 // ECS are highly threaded and we use channels to
 // do the communication anyhow, so only the network
 // stuff needs to run in an async environment.
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
