fe2292e2f22066e07cf7934e8d4d5722ed8cf8b1
Almetica <almetica@protonmail.com>
2020-05-30 14:53:12 +0200

    Add UserLocation model and repository.
    The UserLocation needs to save a rotation value. Since we will work
    with nalgebra, I have decided to save the rotation as a vector.
    
    This may waste some database space (since rotations of players
    are always z_axis() based), but makes working with nalgebra much
    easier (A Rotation3::angle() doesn't provide the full value angle of
    a rotation, so you would need to also use the axis() component, this
    is pretty awkward, so we will just save the scaled axis this way).

--- src/model/migrations/202005181025__user_location.sql
+++ src/model/migrations/202005181025__user_location.sql
@@ -2,8 +2,10 @@ CREATE TABLE "user_location"
 (
     "user_id" INT NOT NULL UNIQUE REFERENCES "user" ON DELETE CASCADE,
     "zone"    INT NOT NULL,
-    "location_x" FLOAT NOT NULL,
-    "location_y" FLOAT NOT NULL,
-    "location_z" FLOAT NOT NULL,
-    "rotation" SMALLINT NOT NULL
+    "location_x" REAL NOT NULL,
+    "location_y" REAL NOT NULL,
+    "location_z" REAL NOT NULL,
+    "rotation_x" REAL NOT NULL,
+    "rotation_y" REAL NOT NULL,
+    "rotation_z" REAL NOT NULL
 );
