f6496ef1b1e634dbee2d5217e13fd8672300acd2
Almetica <almetica@protonmail.com>
2020-05-30 17:24:07 +0200

    Create an UserLocation when creating an User.

--- src/model/repository/user_location.rs
+++ src/model/repository/user_location.rs
@@ -20,18 +20,26 @@ pub async fn create(conn: &mut PgConnection, location: &UserLocation) -> Result<
     .bind(&location.rotation.scaled_axis().x)
     .bind(&location.rotation.scaled_axis().y)
     .bind(&location.rotation.scaled_axis().z)
-    .map(|row: PgRow| UserLocation {
-        user_id: row.get(0),
-        zone: row.get(1),
-        point: Point3::new(row.get(2), row.get(3), row.get(4)),
-        rotation: Rotation3::from_scaled_axis(Vector3::new(row.get(5), row.get(6), row.get(7))),
-    })
+    .map(map_user)
     .fetch_all(conn)
     .await?;
 
     location
         .pop()
         .ok_or(anyhow!("Couldn't find the created UserLocation in row"))
+}
+
+/// Get the location of a user.
+pub async fn get_by_user_id(conn: &mut PgConnection, user_id: i32) -> Result<UserLocation> {
+    let mut location = sqlx::query(r#"SELECT * FROM "user_location" WHERE "user_id" = $1"#)
+        .bind(&user_id)
+        .map(map_user)
+        .fetch_all(conn)
+        .await?;
+
+    location
+        .pop()
+        .ok_or(anyhow!("Couldn't find the get UserLocation in row"))
 }
 
 /// Updates the location of a user.
@@ -50,18 +58,22 @@ pub async fn update(conn: &mut PgConnection, location: &UserLocation) -> Result<
         .bind(&location.rotation.scaled_axis().y)
         .bind(&location.rotation.scaled_axis().z)
         .bind(&location.user_id)
-        .map(|row: PgRow| UserLocation {
-            user_id: row.get(0),
-            zone: row.get(1),
-            point: Point3::new(row.get(2), row.get(3), row.get(4)),
-            rotation: Rotation3::from_scaled_axis(Vector3::new(row.get(5), row.get(6), row.get(7))),
-        })
+        .map(map_user)
         .fetch_all(conn)
         .await?;
 
     location
         .pop()
         .ok_or(anyhow!("Couldn't find the update UserLocation in row"))
+}
+
+fn map_user(row: PgRow) -> UserLocation {
+    UserLocation {
+        user_id: row.get(0),
+        zone: row.get(1),
+        point: Point3::new(row.get(2), row.get(3), row.get(4)),
+        rotation: Rotation3::from_scaled_axis(Vector3::new(row.get(5), row.get(6), row.get(7))),
+    }
 }
 
 #[cfg(test)]
@@ -98,6 +110,33 @@ pub mod tests {
                 };
 
                 let db_location = create(&mut conn, &location).await?;
+
+                assert_eq!(db_location.user_id, location.user_id);
+                assert_eq!(db_location.zone, location.zone);
+                assert_eq!(db_location.point, location.point);
+                assert_relative_eq!(db_location.rotation, location.rotation);
+
+                Ok(())
+            })
+        })
+    }
+
+    #[test]
+    fn test_get_location_by_user_id() -> Result<()> {
+        db_test(|db_string| {
+            task::block_on(async {
+                let mut conn = PgConnection::connect(db_string).await?;
+                let user = setup(&mut conn).await?;
+
+                let location = UserLocation {
+                    user_id: user.id,
+                    zone: 14,
+                    point: Point3::new(1.0f32, 2.0f32, 3.0f32),
+                    rotation: Rotation3::from_axis_angle(&Vector3::z_axis(), 3.0),
+                };
+                create(&mut conn, &location).await?;
+
+                let db_location = get_by_user_id(&mut conn, user.id).await?;
 
                 assert_eq!(db_location.user_id, location.user_id);
                 assert_eq!(db_location.zone, location.zone);
