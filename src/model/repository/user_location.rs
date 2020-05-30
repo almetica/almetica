2c97f44967102f56d896c3d9099e842ef45dd057
Almetica <almetica@protonmail.com>
2020-05-30 16:21:29 +0200

    Simplify UserLocation repository handling.

--- src/model/repository/user_location.rs
+++ src/model/repository/user_location.rs
@@ -1,57 +1,67 @@
 /// Handles the location of an user.
 use crate::model::entity::UserLocation;
 use crate::Result;
-use nalgebra::{Point3, Rotation3};
+use anyhow::anyhow;
+use nalgebra::{Point3, Rotation3, Vector3};
+use sqlx::postgres::PgRow;
 use sqlx::prelude::*;
 use sqlx::PgConnection;
 
 /// Creates a new user location.
-pub async fn create(
-    conn: &mut PgConnection,
-    user_id: i32,
-    zone: i32,
-    point: &Point3<f32>,
-    rotation: &Rotation3<f32>,
-) -> Result<UserLocation> {
-    Ok(sqlx::query_as::<_, UserLocation>(
+pub async fn create(conn: &mut PgConnection, location: &UserLocation) -> Result<UserLocation> {
+    let mut location = sqlx::query(
         r#"INSERT INTO "user_location" VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *"#,
     )
-    .bind(&user_id)
-    .bind(&zone)
-    .bind(&point.x)
-    .bind(&point.y)
-    .bind(&point.z)
-    .bind(&rotation.scaled_axis().x)
-    .bind(&rotation.scaled_axis().y)
-    .bind(&rotation.scaled_axis().z)
-    .fetch_one(conn)
-    .await?)
+    .bind(&location.user_id)
+    .bind(&location.zone)
+    .bind(&location.point.x)
+    .bind(&location.point.y)
+    .bind(&location.point.z)
+    .bind(&location.rotation.scaled_axis().x)
+    .bind(&location.rotation.scaled_axis().y)
+    .bind(&location.rotation.scaled_axis().z)
+    .map(|row: PgRow| UserLocation {
+        user_id: row.get(0),
+        zone: row.get(1),
+        point: Point3::new(row.get(2), row.get(3), row.get(4)),
+        rotation: Rotation3::from_scaled_axis(Vector3::new(row.get(5), row.get(6), row.get(7))),
+    })
+    .fetch_all(conn)
+    .await?;
+
+    location
+        .pop()
+        .ok_or(anyhow!("Couldn't find the created UserLocation in row"))
 }
 
 /// Updates the location of a user.
-pub async fn update(
-    conn: &mut PgConnection,
-    user_id: i32,
-    zone: i32,
-    point: &Point3<f32>,
-    rotation: &Rotation3<f32>,
-) -> Result<UserLocation> {
-    Ok(sqlx::query_as::<_, UserLocation>(
-        r#"UPDATE "user_location" 
+pub async fn update(conn: &mut PgConnection, location: &UserLocation) -> Result<UserLocation> {
+    let mut location = sqlx::query(
+        r#"UPDATE "user_location"
         SET "zone" = $1, "location_x" = $2, "location_y" = $3, "location_z" = $4, "rotation_x" = $5, "rotation_y" = $6, "rotation_z" = $7
         WHERE "user_id" = $8
         RETURNING *"#,
-    )
-    .bind(&zone)
-    .bind(&point.x)
-    .bind(&point.y)
-    .bind(&point.z)
-    .bind(&rotation.scaled_axis().x)
-    .bind(&rotation.scaled_axis().y)
-    .bind(&rotation.scaled_axis().z)
-    .bind(&user_id)
-    .fetch_one(conn)
-    .await?)
+        )
+        .bind(&location.zone)
+        .bind(&location.point.x)
+        .bind(&location.point.y)
+        .bind(&location.point.z)
+        .bind(&location.rotation.scaled_axis().x)
+        .bind(&location.rotation.scaled_axis().y)
+        .bind(&location.rotation.scaled_axis().z)
+        .bind(&location.user_id)
+        .map(|row: PgRow| UserLocation {
+            user_id: row.get(0),
+            zone: row.get(1),
+            point: Point3::new(row.get(2), row.get(3), row.get(4)),
+            rotation: Rotation3::from_scaled_axis(Vector3::new(row.get(5), row.get(6), row.get(7))),
+        })
+        .fetch_all(conn)
+        .await?;
+
+    location
+        .pop()
+        .ok_or(anyhow!("Couldn't find the update UserLocation in row"))
 }
 
 #[cfg(test)]
@@ -63,6 +73,7 @@ pub mod tests {
     use crate::model::repository::{account, user};
     use crate::model::tests::db_test;
     use crate::Result;
+    use approx::assert_relative_eq;
     use async_std::task;
     use nalgebra::Vector3;
     use sqlx::PgConnection;
@@ -79,20 +90,19 @@ pub mod tests {
                 let mut conn = PgConnection::connect(db_string).await?;
                 let user = setup(&mut conn).await?;
 
-                let zone: i32 = 14;
-                let point = Point3::new(1.0f32, 2.0f32, 3.0f32);
-                let rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), 3.0);
+                let location = UserLocation {
+                    user_id: user.id,
+                    zone: 14,
+                    point: Point3::new(1.0f32, 2.0f32, 3.0f32),
+                    rotation: Rotation3::from_axis_angle(&Vector3::z_axis(), 3.0),
+                };
 
-                let db_location = create(&mut conn, user.id, zone, &point, &rotation).await?;
+                let db_location = create(&mut conn, &location).await?;
 
-                assert_eq!(db_location.user_id, user.id);
-                assert_eq!(db_location.zone, zone);
-                assert_eq!(db_location.location_x, point.x);
-                assert_eq!(db_location.location_y, point.y);
-                assert_eq!(db_location.location_z, point.z);
-                assert_eq!(db_location.rotation_x, rotation.scaled_axis().x);
-                assert_eq!(db_location.rotation_y, rotation.scaled_axis().y);
-                assert_eq!(db_location.rotation_z, rotation.scaled_axis().z);
+                assert_eq!(db_location.user_id, location.user_id);
+                assert_eq!(db_location.zone, location.zone);
+                assert_eq!(db_location.point, location.point);
+                assert_relative_eq!(db_location.rotation, location.rotation);
 
                 Ok(())
             })
@@ -106,22 +116,23 @@ pub mod tests {
                 let mut conn = PgConnection::connect(db_string).await?;
                 let user = setup(&mut conn).await?;
 
-                let zone: i32 = 14;
-                let point = Point3::new(1.0f32, 2.0f32, 3.0f32);
-                let rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), 3.0);
-                create(&mut conn, user.id, zone, &point, &rotation).await?;
+                let mut location = UserLocation {
+                    user_id: user.id,
+                    zone: 14,
+                    point: Point3::new(1.0f32, 2.0f32, 3.0f32),
+                    rotation: Rotation3::from_axis_angle(&Vector3::z_axis(), 3.0),
+                };
+                create(&mut conn, &location).await?;
 
-                let new_point = Point3::new(3.0f32, 4.0f32, 5.0f32);
-                let new_location = update(&mut conn, user.id, zone, &new_point, &rotation).await?;
+                location.zone = 22;
+                location.point = Point3::new(3.0f32, 4.0f32, 5.0f32);
+                location.rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), 4.0);
+                let db_location = update(&mut conn, &location).await?;
 
-                assert_eq!(new_location.user_id, user.id);
-                assert_eq!(new_location.zone, zone);
-                assert_eq!(new_location.location_x, new_point.x);
-                assert_eq!(new_location.location_y, new_point.y);
-                assert_eq!(new_location.location_z, new_point.z);
-                assert_eq!(new_location.rotation_x, rotation.scaled_axis().x);
-                assert_eq!(new_location.rotation_y, rotation.scaled_axis().y);
-                assert_eq!(new_location.rotation_z, rotation.scaled_axis().z);
+                assert_eq!(db_location.user_id, location.user_id);
+                assert_eq!(db_location.zone, location.zone);
+                assert_eq!(db_location.point, location.point);
+                assert_relative_eq!(db_location.rotation, location.rotation);
 
                 Ok(())
             })
